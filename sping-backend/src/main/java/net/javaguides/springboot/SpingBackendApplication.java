1f0da162cc1c9448cc54b2fd9c10781bde117482
Jigna123k <110156013+Jigna123k@users.noreply.github.com>
2022-09-22 16:20:40 +0530

    New Filed added and modified

---
+++ sping-backend/src/main/java/net/javaguides/springboot/SpingBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBackendApplication.class, args);
+	}
+
+}
